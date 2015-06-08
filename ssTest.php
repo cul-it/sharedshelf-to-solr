f8525e5d1f06703244681134899c46630dcbe7f2
James Reidy <jreidy@cornell.edu>
2015-06-08 12:05:29 -0400

    check projects and assets

--- ssTest.php
+++ ssTest.php
@@ -5,6 +5,15 @@ require_once('SharedShelfService.php');
 try {
   $ss = new SharedShelfService();
 
+  if ($ss->logged_in()) {
+    echo "User is logged in\n";
+  }
+
+  $projects = $ss->projects();
+  print_r($projects);
+
+  $assets = $ss->project_assets(616);
+  print_r($assets);
 }
 catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
