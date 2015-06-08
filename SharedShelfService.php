8d12218243b36abd3e66db909a4499a60c4f673f
James Reidy <jreidy@cornell.edu>
2015-06-08 12:04:52 -0400

    get response function

--- SharedShelfService.php
+++ SharedShelfService.php
@@ -54,6 +54,28 @@ class SharedShelfService {
     }
   }
 
+  function get_response($url_suffix = '/account') {
+    $url = $this->sharedshelf_url . $url_suffix;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie_jar_path);
+    /* make sure you provide FULL PATH to cookie files*/
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    $output = curl_exec ($ch);
+    curl_close($ch);
+    if ($output === FALSE) {
+      throw new Exception("Error Processing Request: " . $url, 1);
+    }
+    $output = json_decode($output, true);
+    if (isset($output['success']) && ($output['success'] === TRUE)) {
+      return $output;
+    }
+    else {
+      throw new Exception("Error Processing Request: " . $url, 1);
+    }
+  }
+
   function login() {
     $user = parse_ini_file('ssUser.ini');
     if ($user === FALSE) {
