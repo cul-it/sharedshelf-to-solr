a8d8825e1f5842fa1525b00e3ca16aaf59e71fa0
James Reidy <jreidy@cornell.edu>
2015-06-08 13:59:49 -0400

    more testing

--- ssTest.php
+++ ssTest.php
@@ -3,17 +3,32 @@
 require_once('SharedShelfService.php');
 
 try {
-  $ss = new SharedShelfService();
+  $user = parse_ini_file('ssUser.ini');
+  if ($user === FALSE) {
+    throw new Exception("Need to create ssUser.ini. See README.md", 1);
+  }
+
+  $ss = new SharedShelfService($user['email'], $user['password']);
 
   if ($ss->logged_in()) {
     echo "User is logged in\n";
   }
 
   $projects = $ss->projects();
-  print_r($projects);
+  //print_r($projects);
 
-  $assets = $ss->project_assets(616);
-  print_r($assets);
+  $selected_project_id = 616;
+
+  $metadata = $ss->project_fields($selected_project_id);
+  print_r($metadata);
+
+  $assets = $ss->project_assets($selected_project_id);
+  //print_r($assets);
+  $count = count($assets);
+
+  $id = rand(0, $count-1);
+  $asset = $ss->asset($assets["$id"]);
+  //print_r($asset);
 }
 catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
