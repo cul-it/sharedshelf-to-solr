f201715ae1b6fd3c0131a2cc345171b89b105052
James Reidy <jreidy@cornell.edu>
2015-06-09 09:47:17 -0400

    constructor stores url of solr

--- SolrUpdater.php
+++ SolrUpdater.php
@@ -1,4 +1,15 @@
 <?php
 class SolrUpdater {
 
+  private $solr_url = '';
+
+  /**
+   * constructor
+   * @param string $solr URL of the solr service
+   * eg. http://jrc88.solr.library.cornell.edu/solr/
+   */
+  function __construct($solr) {
+    $this->solr_url = $solr;
+  }
+
 }
