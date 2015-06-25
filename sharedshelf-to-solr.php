b5598dcef18afea3dd159b5cd1f5f859d659b10e
James Reidy <jreidy@cornell.edu>
2015-06-25 14:16:26 -0400

    second merge

--- sharedshelf-to-solr.php
+++ sharedshelf-to-solr.php
@@ -5,6 +5,8 @@ require_once('SharedShelfService.php');
 require_once('SolrUpdater.php');
 require_once('SharedShelfToSolrLogger.php');
 
+define("FORCE_REPLACEMENT", FALSE);
+
 $log = FALSE;
 
 try {
@@ -87,13 +89,17 @@ try {
           else {
             $solr_date = trim($solr_in['updated_on_s']);
           }
-          ; $solr_date = FALSE; // *************************debugging******************
-          if ($ss_date == $solr_date) {
+          if (FORCE_REPLACEMENT) {
+            $log->note('Job:Replace');
+          }
+          else if ($ss_date == $solr_date) {
             // dates match - skip this record
             $log->note('Job:Skip-DatesMatch');
             continue;
           }
-          $log->note('Job:Update');
+          else {
+            $log->note('Job:Update');
+          }
           $flattened_asset = $ss->asset_field_values($asset);
           $solr_new = $solr->convert_ss_names_to_solr($flattened_asset);
           $solr_out = array_replace($solr_in, $solr_new);
@@ -105,8 +111,8 @@ try {
       }
       if (!empty($solr_assets)) {
         $result = $solr->add($solr_assets);
-        if ($result != 0) {
-          throw new Exception("Error adding record: $solr_id", 1);
+        if ($result->responseHeader->status != 0) {
+          throw new Exception("Error Processing Request: $err", 1);
         }
       }
     }
