0e22ae0b24e9ddc8c59a79a34625aaee982fb249
James Reidy <jreidy@cornell.edu>
2015-07-21 16:22:30 -0400

    make a local copy of url image to work with

--- iiif-static-image.php
+++ iiif-static-image.php
@@ -1,9 +1,34 @@
 <?php
 // iiif-static-image.php - generate static iiif images
 
+function iiif_static_mkdir($path) {
+  // make a directory at the given path
+  if (!is_dir($path)) {
+    if (mkdir($path, 0775, TRUE) === FALSE) {
+      throw new Exception("Can't create directory : $path", 1);
+    }
+  }
+}
+
 function iiif_static_image($url, $collection_id, $image_id) {
-  $path = "iiif-static-images/$collection_id/$image_id";
-  $command = "/cul/share/sharedshelf-to-solr/iiif-static-image.sh $url $path";
+
+  // create local directory
+  $dir = realpath(dirname(__FILE__));
+  $path = "$dir/iiif-static-images/$collection_id/$image_id";
+  iiif_static_mkdir($path);
+
+  // create a local copy of the image file
+  $temp_path = "/tmp/iiif_static_image/$collection_id";
+  iiif_static_mkdir($temp_path);
+  $ext = pathinfo($url, PATHINFO_EXTENSION);
+  $ext = empty($ext) ? 'jpg' : $ext;
+  $local_copy = "$local_path/$image_id.$ext";
+  if (!copy($url,$local_copy)) {
+    throw new Exception("Can't copy $url to local", 1);
+  }
+
+  $script = "/cul/share/iiif/iiif/iiif_static.py";
+  $command = "python $script $local_copy $path";
   $output = '';
   $return_var = 0;
   $lastline = exec($command, $output, $return_var);
@@ -14,6 +39,7 @@ function iiif_static_image($url, $collection_id, $image_id) {
   }
 }
 
+// code for testing the function
 $url = 'http://stor.artstor.org/stor/55533d5d-a593-44b9-95a8-55dcd2121eb8.jpg';
 $collection_id = 'test';
 $image_id = '201711';
