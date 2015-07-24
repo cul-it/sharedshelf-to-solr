83bba52b2ecc046ac1afcf02b72f4dd51cfd09a2
James Reidy <jreidy@cornell.edu>
2015-07-24 11:30:00 -0400

    output re. deleting tmp files

--- image-to-iiif-s3.php
+++ image-to-iiif-s3.php
@@ -108,7 +108,11 @@ function image_to_iiif_s3($image_url, $s3_path, $force_replacement = FALSE, $sav
   }
 
   // delete temp files
-  if (!$save_tmp_files) {
+  if ($save_tmp_files) {
+    if (OUTPUT) echo "Saving $temp_dir.\n";
+  }
+  else {
+    if (OUTPUT) echo "Deleting $temp_dir.\n";
     system('/bin/rm -rf ' . $temp_dir);
   }
 
