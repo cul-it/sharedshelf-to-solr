4b1e3717433504327b42116458eba155218a279a
James Reidy <jreidy@cornell.edu>
2015-10-16 10:05:17 -0400

    tests for comparing piecemeal processing vs asking for full asset list at once.

--- ssTest.php
+++ ssTest.php
@@ -38,14 +38,48 @@ try {
   $assets = $ss->project_asset_ids($selected_project_id);
   //print_r($assets);
   $count = count($assets);
+  $asset_count = $ss->project_assets_count($selected_project_id);
+  if ($count != $asset_count) {
+    print "ERROR: Asset count mismatch: $count vs. $asset_count\n";
+  }
 
-  $id = rand(0, $count-1);
-  $asset_id = $assets["$id"];
+  $missing = 3857010;
+  if (in_array($missing, $assets)) {
+    print "Found $missing in the returned assset list.\n";
+  }
+  else {
+    print "Asset $missing is missing from the asset list.\n";
+  }
+
+  // try getting all assets one by one
+  $assets_piecemeal = array();
+  for ($offset = 0; $offset < $asset_count; $offset++) {
+    $asset_list =  $ss->project_assets($selected_project_id, $offset, 1);
+    $asset = array_shift($asset_list);
+    $assets_piecemeal[] = $asset['id'];
+  }
+  if (in_array($missing, $assets_piecemeal)) {
+    print "Found $missing in the returned piecemeal assset list.\n";
+  }
+  else {
+    print "Asset $missing is missing from the piecemeal asset list.\n";
+  }
+
+  $piecemeal_count = count($assets_piecemeal);
+  print "collection has $asset_count, piecemeal requests found $piecemeal_count.\n";
+
+  $diff = array_diff($assets, $assets_piecemeal);
+  print "Difference:\n";
+  print_r($diff);
+
+  // $id = rand(0, $count-1);
+  // $asset_id = $assets["$id"];
+  $asset_id = $missing;
   $asset = $ss->asset($asset_id);
   //print_r($asset);
 
   $values = $ss->asset_field_values($asset);
-  //print_r($values);
+  print_r($values);
 
   $url = $ss->media_url($asset_id);
   print_r($url);
@@ -53,7 +87,7 @@ try {
 
   echo "\n\n";
 
-  echo $ss->project_fields_ini($selected_project_id);
+  // echo $ss->project_fields_ini($selected_project_id);
 
   // echo "\n\n";
   // $solr = new SolrUpdater('http://jrc88.solr.library.cornell.edu/solr', 'ss2solr.gamelan.ini');
