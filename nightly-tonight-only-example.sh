f51c68c7bec77558a3db0fa009060510b84c45e1
James Reidy <jreidy@cornell.edu>
2016-02-17 13:52:02 -0500

    example file for --force flag

---
+++ nightly-tonight-only-example.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# save a copy of this file with the filename
+# nightly-tonight-only.sh
+# to use the --force flag for sharedshelf-to-solr.php on
+# tonight's cron run
+
+FORCE_UPDATE=1
