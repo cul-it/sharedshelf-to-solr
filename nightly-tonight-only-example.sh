aee49ee49bd28a9f7e3cffb4f8959ea213231e49
James Reidy <jreidy@cornell.edu>
2016-02-17 13:56:18 -0500

    chmod for execute

--- nightly-tonight-only-example.sh
+++ nightly-tonight-only-example.sh
@@ -2,6 +2,6 @@
 # save a copy of this file with the filename
 # nightly-tonight-only.sh
 # to use the --force flag for sharedshelf-to-solr.php on
-# tonight's cron run
+# tonight's cron run.
 
 FORCE_UPDATE=1
