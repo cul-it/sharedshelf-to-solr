272e86ae3a22f8568f5dddb5e17d5ccf9f81e629
James Reidy <jreidy@cornell.edu>
2016-02-17 13:44:17 -0500

    do the git pull before the nightly-tonight-only.sh load

--- nightly.sh
+++ nightly.sh
@@ -5,6 +5,10 @@ cd "$DIR"
 PHP=`which php`
 GIT=`which git`
 
+# check out the latest master branch
+"$GIT" checkout master
+"$GIT" pull
+
 # pull in the prarameter file
 # nightly-tonight-only.sh looks like this:
 #
