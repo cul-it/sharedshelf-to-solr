4c5b6ba7bade8a5664f62142d278b828bbac853b
James Reidy <jreidy@cornell.edu>
2016-03-01 10:23:26 -0500

    list of collection ss2solr commands

---
+++ nightly-task-list.sh
@@ -0,0 +1,15 @@
+# commands for the nightly sharedshelf to solr run
+# sharedshelf-to-solr.php [--help] [--force] [--no-write] [-p NNN] [-s NNN] [-n NNN]
+# projects (-p):
+# 48 - Campus Artifacts, Art &amp; Memorabilia
+# 78 - NYS Aerial Photographs
+# 370 - Reps Slides
+# 522 - Tamang
+# 589 - Reps Bastides
+# 616 - Gamelan
+# 659 - PJ Mode Map Collection
+# 687 - Beyond the Taj: Architectural Traditions and Landscape Experience in South Asia
+# 746 - Ragamala Paintings
+# 1146 - Sri Lankan Vernacular
+
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 48
