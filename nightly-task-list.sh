3be22246410dd8997514ef764e58bc4561bebe57
James Reidy <jreidy@cornell.edu>
2016-03-01 15:57:06 -0500

    re-order from small collections to larger; remove --no-write flags

--- nightly-task-list.sh
+++ nightly-task-list.sh
@@ -12,13 +12,11 @@
 # x 659 - PJ Mode Map Collection - asset count: 310
 # x 687 - Beyond the Taj: Architectural Traditions and Landscape Experience in South Asia - asset count: 6688
 
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 48 --no-write
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 78 --no-write
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 370 --no-write
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 659
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 616
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 48
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 78
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 522
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 589
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 616
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 659
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 687
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 746
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 1146
