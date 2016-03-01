6166dc12379155f8e384217ad72576067d22cc41
James Reidy <jreidy@cornell.edu>
2016-03-01 15:56:23 -0500

    add counts and published status to list of collections

--- nightly-task-list.sh
+++ nightly-task-list.sh
@@ -1,16 +1,16 @@
 # commands for the nightly sharedshelf to solr run
 # sharedshelf-to-solr.php [--help] [--force] [--no-write] [-p NNN] [-s NNN] [-n NNN]
 # projects (-p):
-# 48 - Campus Artifacts, Art &amp; Memorabilia
-# 78 - NYS Aerial Photographs
-# 370 - Reps Slides
-# 522 - Tamang
-# 589 - Reps Bastides
-# 616 - Gamelan
-# 659 - PJ Mode Map Collection
-# 687 - Beyond the Taj: Architectural Traditions and Landscape Experience in South Asia
-# 746 - Ragamala Paintings
-# 1146 - Sri Lankan Vernacular
+# x means in production (see sharedshelf-status.ini)
+# x 48 - Campus Artifacts, Art &amp; Memorabilia - asset count: 1673
+# 370 - Reps Slides - asset count: 1357
+# x 522 - Tamang - asset count: 2539
+# x 589 - Reps Bastides - asset count: 2652
+# x 616 - Gamelan - asset count: 565
+# x 746 - Ragamala Paintings - asset count: 4123
+# x 78 - NYS Aerial Photographs - asset count: 3390
+# x 659 - PJ Mode Map Collection - asset count: 310
+# x 687 - Beyond the Taj: Architectural Traditions and Landscape Experience in South Asia - asset count: 6688
 
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 48 --no-write
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 78 --no-write
