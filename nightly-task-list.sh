1c074a946e58a1110fd477d8e5736791c0ee3496
Melissa Wallace <mhk33@cornell.edu>
2016-03-21 12:58:26 -0400

    force ragamala and aerialny index

--- nightly-task-list.sh
+++ nightly-task-list.sh
@@ -20,4 +20,4 @@
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 659
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 687
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 746 --force
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 78
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 78 --force
