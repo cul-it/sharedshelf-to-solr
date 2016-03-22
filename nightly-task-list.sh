1c61836d15260afa82d6cf762304aeb8b9b00def
Melissa Wallace <mhk33@cornell.edu>
2016-03-22 11:58:57 -0400

    unforce ragamala and aerial

--- nightly-task-list.sh
+++ nightly-task-list.sh
@@ -19,5 +19,5 @@
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 616
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 659
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 687
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 746 --force
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 78 --force
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 746
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 78
