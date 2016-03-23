ddcae0b85ec90d287dbe8ac6f1f5b57e7b65147d
Melissa Wallace <mhk33@cornell.edu>
2016-03-23 14:45:49 -0400

    force nightly reindex for ragamala and bastides

--- nightly-task-list.sh
+++ nightly-task-list.sh
@@ -20,7 +20,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 370 --skip
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 48 --skip
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 522 --skip
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 589
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 589 --force
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 616
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 659
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 687
