654b7e236a2115832917dd6da7419984a7d0368c
Melissa Wallace <mhk33@cornell.edu>
2016-04-26 10:57:14 -0400

    update collection name and multival fields for artifacts; update nightly tasks

--- nightly-task-list.sh
+++ nightly-task-list.sh
@@ -18,9 +18,9 @@ PHP=`which php`
 # find the directory this script is in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 167 --force
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 167
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 370 --skip
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 48
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 48 --force
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 522 --skip
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 589
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 616
@@ -30,5 +30,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 78
 
 # example of creating or updating the IIIF images for a Collection
-"$PHP" "${DIR}/sharedshelf-to-iiif-s3.php" -p 48
-"$PHP" "${DIR}/sharedshelf-to-iiif-s3.php" -p 167
+# "$PHP" "${DIR}/sharedshelf-to-iiif-s3.php" -p 48
