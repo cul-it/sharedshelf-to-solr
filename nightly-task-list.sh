adf53c1806a6f2f7a628a9abcd6d2c294b9f0739
Melissa Wallace <mhk33@cornell.edu>
2016-04-26 12:30:28 -0400

    hip hop creator delimiter; add hip hop to nightly task

--- nightly-task-list.sh
+++ nightly-task-list.sh
@@ -18,7 +18,7 @@ PHP=`which php`
 # find the directory this script is in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 167
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 167 --force
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 370 --skip
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 48 --force
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 522 --skip
