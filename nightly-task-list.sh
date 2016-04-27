66ffc3605936f0bf7bab955190b077dd05c82e1a
Melissa Wallace <mhk33@cornell.edu>
2016-04-27 12:00:06 -0400

    update location fields for hip hop; update nightly task list

--- nightly-task-list.sh
+++ nightly-task-list.sh
@@ -18,9 +18,9 @@ PHP=`which php`
 # find the directory this script is in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 167 --force
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 167
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 370 --skip
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 48 --force
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 48
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 522 --skip
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 589
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 616
