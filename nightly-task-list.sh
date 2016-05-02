78a298fd9f707e6b21b78a8d66042b3133220ac1
Melissa Wallace <mhk33@cornell.edu>
2016-05-02 10:19:56 -0400

    update nightly task list

--- nightly-task-list.sh
+++ nightly-task-list.sh
@@ -29,7 +29,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 687
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 746
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 78
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 97 --force
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 97
 
 # example of creating or updating the IIIF images for a Collection
-"$PHP" "${DIR}/sharedshelf-to-iiif-s3.php" -p 97
+# "$PHP" "${DIR}/sharedshelf-to-iiif-s3.php" -p 97
