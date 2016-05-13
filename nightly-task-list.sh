e126a2169969b13c22126a308b3cf23c7f5c386a
Melissa Wallace <mhk33@cornell.edu>
2016-05-13 17:40:58 -0400

    add racker, eleusis, howell and johnclairmiller; update task list

--- nightly-task-list.sh
+++ nightly-task-list.sh
@@ -2,6 +2,8 @@
 # sharedshelf-to-solr.php [--help] [--force] [--no-write] [--use-dev-solr] [--skip] [-p NNN] [-s NNN] [-n NNN]
 # projects (-p):
 # see sharedshelf-to-solr.ini projects - commented out ones are NOT in production - use --skip flag
+# 112 - Howell Icelandic - asset count: 416
+# 166 - Mysteries at Eleusis - asset count: 847
 # 167 - Hip Hop Flyers - asset count: 494
 # 190 - Joe Conzo Jr. Archive - asset count: 7620
 # 370 - Reps Slides - asset count: 1357
@@ -12,9 +14,11 @@
 # 616 - Gamelan - asset count: 565
 # 659 - PJ Mode Map Collection - asset count: 310
 # 685 - Andrew Dickson White Architectural Photographs Collection - asset count: 1364
+# 657 - John Clair Miller - asset count: 240
 # 687 - Beyond the Taj: Architectural Traditions and Landscape Experience in South Asia - asset count: 6688
 # 746 - Ragamala Paintings - asset count: 4123
 # 78 - NYS Aerial Photographs - asset count: 3390
+# 920 - Efraim Racker Art Albums - asset count: 81
 # 97 - Cornell Cast Collection - asset count: 897
 
 PHP=`which php`
@@ -22,6 +26,8 @@ PHP=`which php`
 # find the directory this script is in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 112
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 166
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 167
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 190
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 370 --skip
@@ -30,12 +36,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 531
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 589
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 616
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 657
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 659
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 685
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 687
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 746
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 78
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 920
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 97
 
 # example of creating or updating the IIIF images for a Collection
-# "$PHP" "${DIR}/sharedshelf-to-iiif-s3.php" -p 531
+"$PHP" "${DIR}/sharedshelf-to-iiif-s3.php" -p 112
+"$PHP" "${DIR}/sharedshelf-to-iiif-s3.php" -p 166
+"$PHP" "${DIR}/sharedshelf-to-iiif-s3.php" -p 657
