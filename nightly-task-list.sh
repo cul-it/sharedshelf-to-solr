d97a9898a9d9bd84e2c02f18d5e97e396be75a77
Melissa Wallace <mhk33@cornell.edu>
2016-05-20 12:01:18 -0400

    DIGCOLL-617: multival for kinsgsbury and update iiif

--- nightly-task-list.sh
+++ nightly-task-list.sh
@@ -47,9 +47,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 687
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 746
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 78
-"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 88
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 88 --force
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 920
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 97
 
 # example of creating or updating the IIIF images for a Collection
-"$PHP" "${DIR}/sharedshelf-to-iiif-s3.php" -p 88
+# "$PHP" "${DIR}/sharedshelf-to-iiif-s3.php" -p 88
