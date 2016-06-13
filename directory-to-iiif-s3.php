5fe7c4f6413c3f67ac1f6a5a331aab0879a4b78d
James Reidy <jreidy@cornell.edu>
2016-06-13 16:00:38 -0400

    initial converter for directories of images

---
+++ directory-to-iiif-s3.php
@@ -0,0 +1,60 @@
+<?php
+// directory-to-iiif-s3.php - convert all the image files in a directory to iiif
+require_once('image-to-iiif-s3.php');
+
+function usage() {
+  global $argv;
+  echo PHP_EOL;
+  echo "Usage: php " . $argv[0] . " [--help] [--force] -f /path/to/files -c collection " . PHP_EOL;
+  echo "--help - show this info" . PHP_EOL;
+  echo "--force - ignore existing and rewrite all iiif images" . PHP_EOL;
+  echo "-f - path to local directory of image files" . PHP_EOL;
+  echo "-c - name of the collection (used in the s3 path)" . PHP_EOL;
+  exit (0);
+}
+
+$log = FALSE;
+
+$options = getopt("f:c:",array("help", "force"));
+
+if (isset($options['help'])) {
+  usage();
+}
+
+$force_replacement = isset($options["force"]);
+if (isset($options['f'])) {
+  $directory = $options['f'];
+}
+else {
+  usage();
+}
+if (isset($options['c'])) {
+  $collection = $options['c'];
+}
+else {
+  usage();
+}
+
+try {
+  $dir = new DirectoryIterator($directory);
+  foreach ($dir as $fileinfo) {
+    if ($fileinfo->isFile()) {
+      $extension = $fileinfo->getExtension();
+      if (in_array($extension, array('png', 'jpg', 'gif', 'tif'))) {
+        $image_url = $fileinfo->getPathname();
+        $filename = $fileinfo->getBasename(".$extension");
+        $s3_path = "$collection/$filename";
+        print "$image_url -> $s3_path\n";
+        image_to_iiif_s3($image_url, $extension, $s3_path, $force_replacement);
+      }
+    }
+  }
+}
+catch (Exception $e) {
+  $error = 'Caught exception: ' . $e->getMessage() . "\n";
+  echo $error;
+  exit (1);
+}
+exit (0);
+
+?>
