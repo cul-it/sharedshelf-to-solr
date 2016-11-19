670930e2c5e83ac2e629207d62172d7bafc7954a
James Reidy <jreidy@cornell.edu>
2016-11-19 08:13:30 -0500

    function to follow and track redirects until there are no more redirects

--- SharedShelfService.php
+++ SharedShelfService.php
@@ -100,6 +100,22 @@ class SharedShelfService {
     }
     return $url;
   }
+
+  /**
+   * take the last url in the array, if it redirects, add new url to array
+   * @param  array $url_array urls
+   * @return boolean            return TRUE if last url redirects
+   */
+  private function follow_redirects(&$url_array) {
+    if (!is_array($url_array)) {
+      throw new Exception("follow_redirects array required", 1);
+    }
+    $last_url = end($url_array);
+    if ($last_url === FALSE) {
+      throw new Exception("follow_redirects empty array", 1);
+    }
+
+    $ch = curl_init($last_url);
     if ($ch === FALSE) {
       curl_close($ch);
       throw new Exception("Bad request url in get_url: $url", 1);
@@ -114,38 +130,20 @@ class SharedShelfService {
       );
     $allswell = curl_setopt_array($ch, $options);
     if ($allswell === FALSE) {
-      throw new Exception("get_url failed to set curl options", 1);
+      throw new Exception("follow_redirects failed to set curl options", 1);
     }
     $response = curl_exec($ch);
-    preg_match_all('/^Location:(.*)$/mi', $response, $matches);
-    $url2 = !empty($matches[1]) ? trim($matches[1][0]) : 'No redirect found';
-    curl_close($ch);
-
-    $extension = pathinfo($url2, PATHINFO_EXTENSION);
-    if (!empty($extension) || $require_extension === FALSE) {
-      return $url2;
-    }
-
-    // $url2 comes back without an extension (eg. .jpg) so get second redirect
-    $ch = curl_init($url2);
-    if ($ch === FALSE) {
-      curl_close($ch);
-      throw new Exception("Bad second request url: $url2", 1);
-    }
-    // re-use curl options
-    $allswell = curl_setopt_array($ch, $options);
-    if ($allswell === FALSE) {
-      throw new Exception("get_url failed to set curl options 2", 1);
-    }
-    $response = curl_exec($ch);
-    curl_close($ch);
     preg_match_all('/^Location:(.*)$/mi', $response, $matches);
     if (empty($matches[1])) {
-      throw new Exception("Can't find url for $url", 1);
+      $redirected = FALSE;
     }
+    else {
+      $redirected = TRUE;
+      $url_array[] = trim($matches[1][0]); // append the url redirected to to the array
+    }
+    curl_close($ch);
 
-    $url_out = trim($matches[1][0]);
-    return $url_out;
+    return $redirected;
   }
 
   function login() {
