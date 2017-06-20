0c8751d7f1f49b547d5bdf26b0d2aa7568c5ace3
James Reidy <jreidy@cornell.edu>
2017-06-20 11:13:38 -0400

    Add functions to get id count and grab document ids in chunks

--- SolrUpdater.php
+++ SolrUpdater.php
@@ -282,6 +282,27 @@ class SolrUpdater {
     return $status;
   }
 
+  function get_count() {
+    $q = "*:*&wt=json&start=0&rows=1&fl=id";
+    $json = $this->get('/select', $q);
+    $result = json_decode($json);
+    $found = isset($result->response->numFound) ? $result->response->numFound : 0;
+    return $found;
+  }
+
+  function get_ids($start = 0, $max_to_find = 99999) {
+    $q = "q=*:*&wt=json&start=$start&rows=$max_to_find&fl=id";
+    $json = $this->get('/select', $q);
+    $result = json_decode($json);
+    $ids = array();
+    if (!empty($result->response->docs)) {
+      foreach ($result->response->docs as $doc) {
+        $ids[] = $doc->id;
+        }
+    }
+    return $ids;
+  }
+
   function get_all_ids($project_id, $max_to_find = 99999) {
     $q = "q=project_id_ssi:$project_id&wt=json&start=0&rows=$max_to_find&fl=id";
     $json = $this->get('/select', $q);
