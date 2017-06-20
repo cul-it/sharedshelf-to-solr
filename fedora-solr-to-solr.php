0e52cc47ef927ff90c6ee4b8ca20659d324024f1
James Reidy <jreidy@cornell.edu>
2017-06-20 11:12:16 -0400

    Set up to process entire Fedora collection

--- fedora-solr-to-solr.php
+++ fedora-solr-to-solr.php
@@ -19,17 +19,34 @@ try {
   $portal_solr = $vars['solr'];
   $solr = new SolrUpdater($portal_solr, $portal_ini);
 
-  $ids = $solr_in->get_all_ids_prefix_type('chla', 'Book', 0, 10);
+  $log = new SharedShelfToSolrLogger('Fedora-to-solr');
+  $log->task('Import_All');
 
-  $assets = array();
-  foreach ($ids as $id) {
-    $doc = $solr_in->get_item($id);
-    $assets[] = $doc;
-    echo 'id: ' . $doc['id'] . PHP_EOL;
+  $count = $solr_in->get_count();
+  $per_call = 10;
+  $log->note("asset_count:$count at $per_call per call");
+
+  echo "Processing: $project asset count: $count " . $log->log_file_name() . PHP_EOL;
+
+  $counter = 0;
+  for ($start = 0; $start < $count; $start += $per_call) {
+
+    $ids = $solr_in->get_all_ids_prefix_type('chla', 'Book', $start, $per_call);
+
+    $assets = array();
+    foreach ($ids as $id) {
+      $doc = $solr_in->get_item($id);
+      $assets[] = $doc;
+      echo 'id: ' . $doc['id'] . PHP_EOL;
+    }
+
+    $solr->add_without_custom($assets);
+
+    $log->item("Last item $id");
+    $pct = sprintf("%01.2f", $counter++ * 100.0 / (float) $count);
+    $log->note("Completed:$pct");
   }
-
-  $solr->add_without_custom($assets);
-
+  $log->task('Done.');
 }
 catch (Exception $e) {
   $error = 'Caught exception: ' . $e->getMessage() . "\n";
