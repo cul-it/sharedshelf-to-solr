cdf16664e7c1090493576c6a4430d7b03779e338
James Reidy <jreidy@cornell.edu>
2017-06-21 10:31:02 -0400

    Remove the count check.

--- fedora-solr-to-solr.php
+++ fedora-solr-to-solr.php
@@ -35,10 +35,6 @@ try {
   echo implode(PHP_EOL, $status) . PHP_EOL;
 
   $count = $solr_in->get_count($selection);
-  if ($count > 2454052) {
-    throw new Exception("Count $count > 2454052", 1);
-  }
-  die ('now');
 
   $per_call = 10;
   $log->note("asset_count:$count at $per_call per call");
