fe43b8c182f23ee474895e68245918c37d623ec4
James Reidy <jreidy@cornell.edu>
2017-07-07 13:25:27 -0400

    Test for extraction of text from pdf

---
+++ extract-test.php
@@ -0,0 +1,35 @@
+<?php
+// extract-test.php - testing extract of text from doc, pdf etc.
+
+require_once('SolrUpdater.php');
+require_once('SharedShelfToSolrLogger.php');
+
+try {
+
+  $ini = 'extract-test.ini';
+  $vars = parse_ini_file($ini);
+  $dev_solr = $vars['solr'];
+  $project = $vars['project'];
+  $solr = new SolrUpdater($dev_solr, $ini);
+
+  // confirm current contents of doc1
+  $doc1 = $solr->get_item('doc1');
+  print_r($doc1);
+
+  $q="literal.id=doc2&stream.file=/tmp/jgr25/FacultyGrants_2013.pdf&stream.contentType=application/pdf&wt=json&debugQuery=on";
+  $json = $solr->raw_get('/update/extract', $q);
+  $result = json_decode($json);
+  print_r(array('result', 'json' => $json, 'php' => $result));
+
+  $solr->commit();
+
+  $doc2 = $solr->get_item('doc2');
+  print_r($doc2);
+}
+catch (Exception $e) {
+  $error = 'Caught exception: ' . $e->getMessage() . "\n";
+  echo $error;
+  $log->task('Done with ERROR.');
+  exit (1);
+}
+exit (0);
