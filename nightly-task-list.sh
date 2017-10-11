a1f38d269e49cf68161b4acfc33be417307afc56
starsplatter <jenncd@gmail.com>
2017-10-11 12:04:50 -0400

    iwo nightly

--- nightly-task-list.sh
+++ nightly-task-list.sh
@@ -102,6 +102,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 922
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 97
 "$PHP" "${DIR}/sharedshelf-to-solr.php" -p 98
+"$PHP" "${DIR}/sharedshelf-to-solr.php" -p 3450
 
 # example of creating or updating the IIIF images for a Collection
 # "$PHP" "${DIR}/sharedshelf-to-iiif-s3.php" -p 3596
