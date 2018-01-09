a94d72be1514002f00985427549d2f4c0460c9c7
Melissa Wallace <mhk33@cornell.edu>
2018-01-09 08:57:18 -0500

    clear nightly task list

--- nightly-task-list.sh
+++ nightly-task-list.sh
@@ -119,4 +119,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 # example of creating or updating the IIIF images for a Collection
-"$PHP" "${DIR}/sharedshelf-to-iiif-s3.php" -p 3262
+# "$PHP" "${DIR}/sharedshelf-to-iiif-s3.php" -p 3262
