f799b8a69a3636622fbd824c6bfbb24f3ac5be16
James Reidy <jreidy@cornell.edu>
2018-01-12 15:53:04 -0500

    List collections lists for each communitiy

--- ecommons/ListCollections.php
+++ ecommons/ListCollections.php
@@ -2,15 +2,20 @@
 require_once('eCommonsService.php');
 
 try {
-    $user = parse_ini_file('ecommons.ini');
-
     $ecommons = new eCommonsService();
 
-    $test = $ecommons->get_response('/test');
+    $test = $ecommons->get_response('/test', FALSE);
     print_r($test);
 
-    $communities = $ecommons->get_response('/communities');
-    print_r($communities);
+    $communities = $ecommons->get_response('/communities', FALSE);
+    $indent = '    ';
+    foreach ($communities as $id => $community) {
+        echo $community['name'] . PHP_EOL;
+        $collections = $ecommons->get_response('/communities/' . $community['id'] . '/collections');
+        foreach ($collections as $col) {
+            echo $indent . $col['id'] . ' ' . $col['name'] . PHP_EOL;
+        }
+    }
 
 }
 catch (Exception $e) {
