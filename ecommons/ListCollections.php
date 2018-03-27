cb750999dd9f577f33204a48a75e05e51b3cd63e
James Reidy <jreidy@cornell.edu>
2018-03-27 17:04:58 -0400

    add community ID to ListCollections

--- ecommons/ListCollections.php
+++ ecommons/ListCollections.php
@@ -7,7 +7,7 @@ try {
     $communities = $ecommons->get_response('/communities', FALSE);
     $indent = '    ';
     foreach ($communities as $id => $community) {
-        echo $community['name'] . PHP_EOL;
+        echo $community['id'] . ' ' . $community['name'] . PHP_EOL;
         $collections = $ecommons->get_response('/communities/' . $community['id'] . '/collections');
         foreach ($collections as $col) {
             echo $indent . $col['id'] . ' ' . $col['name'] . PHP_EOL;
