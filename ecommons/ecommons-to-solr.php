138faefb6c7dcc8216b434793adad0926fe677cb
James Reidy <jreidy@cornell.edu>
2018-03-30 12:01:08 -0400

    flesh out the code

--- ecommons/ecommons-to-solr.php
+++ ecommons/ecommons-to-solr.php
@@ -33,50 +33,126 @@ function usage() {
     exit (0);
 }
 
-try {
-    $options = getopt("c:s:n:",array("help", "force", "no-write"));
+function flatten($asset_array) {
+    $asset = array();
+    foreach($asset_array as $item) {
+        $key = $item['key'];
+        $asset["$key"] = $item['value'];
+    }
+    return $asset;
+}
 
-    if ($options === false || empty($options) || isset($options['help'])) {
+$options = getopt("c:s:n:",array("help", "force", "no-write"));
+
+if ($options === false || empty($options) || isset($options['help'])) {
+    usage();
+}
+$force_replacement = isset($options["force"]);
+$do_not_write_to_solr = isset($options["no-write"]);
+if (isset($options['c'])) {
+    if (is_numeric($options['c'])) {
+        $single_collection = $options['c'];
+    }
+    else {
         usage();
     }
-    $force_replacement = isset($options["force"]);
-    $do_not_write_to_solr = isset($options["no-write"]);
-    if (isset($options['c'])) {
-        if (is_numeric($options['c'])) {
-            $single_collection = $options['c'];
-        }
-        else {
-            usage();
-        }
+}
+else {
+    usage();
+}
+if (isset($options['s'])) {
+    if (is_numeric($options['s'])) {
+        $starting_asset = $options['s'];
     }
     else {
-        $single_collection = FALSE;
+        usage();
     }
-    if (isset($options['s'])) {
-        if (is_numeric($options['s'])) {
-            $starting_asset = $options['s'];
-        }
-        else {
-            usage();
-        }
-    }
-    else {
-        $starting_asset = 0;
-    }
-    if (isset($options['n'])) {
-        if (is_numeric($options['n'])) {
-            $max_processing_count = $options['n'];
-        }
-        else {
-            usage();
-        }
+}
+else {
+    $starting_asset = 0;
+}
+if (isset($options['n'])) {
+    if (is_numeric($options['n'])) {
+        $max_processing_count = $options['n'];
     }
     else {
-        $max_processing_count = false; // this means process them all
+        usage();
     }
+}
+else {
+    $max_processing_count = false; // this means process them all
+}
+
+
+try {
+    $project_id = $single_collection;
+
+    // load the mapping
+    $config = "ecommons.ini";
+    $task = parse_ini_file($config, TRUE);
+    if ($task === FALSE) {
+        throw new Exception("Need configuration file: $config", 1);
+    }
+    
+    // connect to solr
+    $solr_url = $task['solr'];
+    $solr = new SolrUpdater($solr_url, $config);
+
+    // list of the ids already in solr
+    $solr_asset_id_list = $solr->get_all_ids($project_id);
+    $solr_asset_ids_to_delete = array_flip($solr_asset_id_list);
+    echo("Ids in solr for project $project_id: " . count($solr_asset_id_list) . PHP_EOL);
 
     $ecommons = new eCommonsService();
+
+    // get collection info
+    $collection = $ecommons->get_response('/collections/' . $project_id );
+    if (empty($collection)) {
+        throw new Exception("No info for collection $project_id ", 1);               
+    }
+    if (empty($collection['numberItems'])) {
+        throw new Exception("Collection $project_id  has no items", 1);               
+    }
+    $numberItems = $collection['numberItems'];
+    echo("Items in collection: $numberItems\n");
+    $collectionName = $collection['name'];
+    echo("Collection name: $collectionName\n");
     
+    $pagecount = min(10, $numberItems);
+    for ($offset = 0; $offset < $numberItems; $offset += $pagecount) {
+        $items = $ecommons->get_response("/collections/$project_id/items?limit=$pagecount&offset=$offset");
+        if (empty($items)) {
+            throw new Exception("No items from offset $offset on collection $single_collection", 1);
+        }
+        foreach ($items as $item) {
+            $asset_id = $item['id'];
+            $solr_id = ECOMMONS_ID_PREFIX . $asset_id;
+            // track ids dropped from ecommons
+            unset($solr_asset_ids_to_delete["$solr_id"]);
+
+            $asset = $ecommons->get_response('/items/' . $asset_id . '/metadata');
+            if (empty($asset)) {
+                throw new Exception("No metadata for item $asset_id", 1);               
+            }
+            echo("Solr id: $solr_id\n");
+            $asset = flatten($asset);
+            $merged = array_merge($asset, $item); // item and metadata in one flat array
+            $solr_out = $solr->convert_ss_names_to_solr($merged);
+            $solr_out['id'] = $solr_id; // switch to solr id from ecommons
+            $solr_out['collection_tesim'] = $collectionName; // can not use set_solr_field[collection_tesim]
+            $solr_assets = array($solr_out);
+            $result = $solr->add($solr_assets);
+        }
+    }
+
+    if (!empty($solr_asset_ids_to_delete)) {
+        $ids = array_flip($solr_asset_ids_to_delete);
+        echo("Deleting solr ids: " . implode(', ', $ids));
+        $solr->delete_items($ids);
+    }
+    else {
+        echo("No solr asssets to delete for project $project_id.\n");
+    }
 }
 catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
