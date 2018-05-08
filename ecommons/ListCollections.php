6c8c8a2ad1ddc76c628b390a053e41506598b438
James Reidy <jreidy@cornell.edu>
2018-05-08 16:38:10 -0400

    partial change for uuid

--- ecommons/ListCollections.php
+++ ecommons/ListCollections.php
@@ -7,8 +7,11 @@ try {
     $communities = $ecommons->get_response('/communities', FALSE);
     $indent = '    ';
     foreach ($communities as $id => $community) {
-        echo $community['id'] . ' ' . $community['name'] . PHP_EOL;
-        $collections = $ecommons->get_response('/communities/' . $community['id'] . '/collections');
+        echo $community['uuid'] . ' ' . $community['name'] . PHP_EOL;
+        $collections = $ecommons->get_response('/communities/' . $community['uuid'] . '/collections');
+        print_r($community);
+        print_r($collections);
+        die('here');
         foreach ($collections as $col) {
             echo $indent . $col['id'] . ' ' . $col['name'] . PHP_EOL;
         }
