83d27740cf0d6d22ff21240fe66b680598c167ab
James Reidy <jreidy@cornell.edu>
2018-05-09 10:48:03 -0400

    create list of ecommons collections and run loader

---
+++ ecommons/ecommons-tasks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PHP=`which php`
+
+# find the directory this script is in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# create a new list of eCommons collections
+"$PHP" "${DIR}/generate-collection-list.php" > "${DIR}/collection-list.txt"
+
+# load each collection into solr
+. "${DIR}/load-ecommons.sh"
+
