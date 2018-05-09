1b315920374a8bea7dac68d088b96cf8c0193d74
James Reidy <jreidy@cornell.edu>
2018-05-09 11:27:44 -0400

    permissions changes

--- ecommons/ecommons_nightly.sh
+++ ecommons/ecommons_nightly.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash 
 
 # find the directory this script is in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # run the task list
-. "${DIR}/nightly-task-list.sh"
+. "${DIR}/ecommons-tasks.sh"
