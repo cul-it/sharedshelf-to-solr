70d540e2d43b70aeb607b10278b1f79bec2c1cfc
James Reidy <jreidy@cornell.edu>
2018-07-06 12:01:42 -0400

    start of class for handling MAP data

---
+++ SharedShelfMap/SharedShelfMetadataApplicationProfile.php
@@ -0,0 +1,138 @@
+<?php
+require_once('../SharedShelfService.php');
+
+class SharedShelfMetadataApplicationProfile {
+
+    private $sss = '';   // instance of SharedShelfService
+    private $project = FALSE;
+    private $metadata = FALSE;
+    private $project_fields = FALSE;
+
+    private $map_fields = array(
+        "Address" => array( 'map_name' => "Address", 'solr_name' => "map_address", 'multivalued' => TRUE, 'type' => "string" ),
+        "Agent" => array( 'map_name' => "Agent", 'solr_name' => "map_agent", 'multivalued' => TRUE, 'type' => "string" ),
+        "Agent_Role" => array( 'map_name' => "Agent_Role", 'solr_name' => "map_agent_role", 'multivalued' => FALSE, 'type' => "string" ),
+        "Alternate Title" => array( 'map_name' => "Alternate Title", 'solr_name' => "map_alternate_title", 'multivalued' => FALSE, 'type' => "string" ),
+        "Annotation" => array( 'map_name' => "Annotation", 'solr_name' => "map_annotation", 'multivalued' => FALSE, 'type' => "string" ),
+        "Archival Collection" => array( 'map_name' => "Archival Collection", 'solr_name' => "map_archival_collection", 'multivalued' => FALSE, 'type' => "string" ),
+        "Archival Finding Aid" => array( 'map_name' => "Archival Finding Aid", 'solr_name' => "map_archival_finding_aid", 'multivalued' => FALSE, 'type' => "string" ),
+        "Artstor Classification Display" => array( 'map_name' => "Artstor Classification Display", 'solr_name' => "map_artstor_classification_display", 'multivalued' => FALSE, 'type' => "string" ),
+        "Artstor Classification Facet" => array( 'map_name' => "Artstor Classification Facet", 'solr_name' => "map_artstor_classification_facet", 'multivalued' => FALSE, 'type' => "string" ),
+        "Artstor Classification Link" => array( 'map_name' => "Artstor Classification Link", 'solr_name' => "map_artstor_classification_link", 'multivalued' => FALSE, 'type' => "string" ),
+        "Artstor Collection Digital Publisher" => array( 'map_name' => "Artstor Collection Digital Publisher", 'solr_name' => "map_artstor_collection_digital_publisher", 'multivalued' => FALSE, 'type' => "string" ),
+        "Artstor Collection Name" => array( 'map_name' => "Artstor Collection Name", 'solr_name' => "map_artstor_collection_name", 'multivalued' => FALSE, 'type' => "string" ),
+        "Artstor Collection Publication Date" => array( 'map_name' => "Artstor Collection Publication Date", 'solr_name' => "map_artstor_collection_publication_date", 'multivalued' => FALSE, 'type' => "date" ),
+        "Artstor Collection Status" => array( 'map_name' => "Artstor Collection Status", 'solr_name' => "map_artstor_collection_status", 'multivalued' => FALSE, 'type' => "string" ),
+        "ARTstor Id" => array( 'map_name' => "ARTstor Id", 'solr_name' => "map_artstor_id", 'multivalued' => FALSE, 'type' => "string" ),
+        "Bibliography" => array( 'map_name' => "Bibliography", 'solr_name' => "map_bibliography", 'multivalued' => TRUE, 'type' => "string" ),
+        "Box" => array( 'map_name' => "Box", 'solr_name' => "map_box", 'multivalued' => FALSE, 'type' => "string" ),
+        "Cataloger" => array( 'map_name' => "Cataloger", 'solr_name' => "map_cataloger", 'multivalued' => FALSE, 'type' => "string" ),
+        "Cite As" => array( 'map_name' => "Cite As", 'solr_name' => "map_cite_as", 'multivalued' => FALSE, 'type' => "string" ),
+        "Collection Level Bib" => array( 'map_name' => "Collection Level Bib", 'solr_name' => "map_collection_level_bib", 'multivalued' => FALSE, 'type' => "string" ),
+        "Collection Sequence" => array( 'map_name' => "Collection Sequence", 'solr_name' => "map_collection_sequence", 'multivalued' => FALSE, 'type' => "string" ),
+        "Collection Website" => array( 'map_name' => "Collection Website", 'solr_name' => "map_collection_website", 'multivalued' => FALSE, 'type' => "string" ),
+        "Condition" => array( 'map_name' => "Condition", 'solr_name' => "map_condition", 'multivalued' => FALSE, 'type' => "string" ),
+        "Country" => array( 'map_name' => "Country", 'solr_name' => "map_country", 'multivalued' => TRUE, 'type' => "string" ),
+        "Culture" => array( 'map_name' => "Culture", 'solr_name' => "map_culture", 'multivalued' => TRUE, 'type' => "string" ),
+        "Date" => array( 'map_name' => "Date", 'solr_name' => "map_date", 'multivalued' => TRUE, 'type' => "tdate" ),
+        "Date_Type" => array( 'map_name' => "Date_Type", 'solr_name' => "map_date_type", 'multivalued' => FALSE, 'type' => "string" ),
+        "DCMI Type" => array( 'map_name' => "DCMI Type", 'solr_name' => "map_dcmi_type", 'multivalued' => FALSE, 'type' => "string" ),
+        "Description" => array( 'map_name' => "Description", 'solr_name' => "map_description", 'multivalued' => TRUE, 'type' => "string" ),
+        "Disable Download" => array( 'map_name' => "Disable Download", 'solr_name' => "map_disable_download", 'multivalued' => FALSE, 'type' => "int" ),
+        "Earliest Date" => array( 'map_name' => "Earliest Date", 'solr_name' => "map_earliest_date", 'multivalued' => FALSE, 'type' => "int" ),
+        "Elevation" => array( 'map_name' => "Elevation", 'solr_name' => "map_elevation", 'multivalued' => FALSE, 'type' => "string" ),
+        "Event" => array( 'map_name' => "Event", 'solr_name' => "map_event", 'multivalued' => TRUE, 'type' => "string" ),
+        "Exhibition" => array( 'map_name' => "Exhibition", 'solr_name' => "map_exhibition", 'multivalued' => TRUE, 'type' => "string" ),
+        "Folder" => array( 'map_name' => "Folder", 'solr_name' => "map_folder", 'multivalued' => FALSE, 'type' => "string" ),
+        "Identifier" => array( 'map_name' => "Identifier", 'solr_name' => "map_identifier", 'multivalued' => TRUE, 'type' => "string" ),
+        "Identifier_Type" => array( 'map_name' => "Identifier_Type", 'solr_name' => "map_identifier_type", 'multivalued' => FALSE, 'type' => "string" ),
+        "Image View Description" => array( 'map_name' => "Image View Description", 'solr_name' => "map_image_view_description", 'multivalued' => TRUE, 'type' => "string" ),
+        "Image View Type" => array( 'map_name' => "Image View Type", 'solr_name' => "map_image_view_type", 'multivalued' => TRUE, 'type' => "string" ),
+        "Inscription" => array( 'map_name' => "Inscription", 'solr_name' => "map_inscription", 'multivalued' => TRUE, 'type' => "string" ),
+        "isTranslatedAs" => array( 'map_name' => "isTranslatedAs", 'solr_name' => "map_istranslatedas", 'multivalued' => TRUE, 'type' => "int" ),
+        "isTranslationOf" => array( 'map_name' => "isTranslationOf", 'solr_name' => "map_istranslationof", 'multivalued' => TRUE, 'type' => "int" ),
+        "Kaltura ID" => array( 'map_name' => "Kaltura ID", 'solr_name' => "map_kaltura_id", 'multivalued' => TRUE, 'type' => "string" ),
+        "Kaltura Playlist" => array( 'map_name' => "Kaltura Playlist", 'solr_name' => "map_kaltura_playlist", 'multivalued' => FALSE, 'type' => "string" ),
+        "Keywords" => array( 'map_name' => "Keywords", 'solr_name' => "map_keywords", 'multivalued' => TRUE, 'type' => "string" ),
+        "Language" => array( 'map_name' => "Language", 'solr_name' => "map_language", 'multivalued' => TRUE, 'type' => "string" ),
+        "Latest Date" => array( 'map_name' => "Latest Date", 'solr_name' => "map_latest_date", 'multivalued' => FALSE, 'type' => "int" ),
+        "Latitude" => array( 'map_name' => "Latitude", 'solr_name' => "map_latitude", 'multivalued' => FALSE, 'type' => "location" ),
+        "Legacy_Label" => array( 'map_name' => "Legacy_Label", 'solr_name' => "map_legacy_label", 'multivalued' => TRUE, 'type' => "string" ),
+        "Legacy_Value" => array( 'map_name' => "Legacy_Value", 'solr_name' => "map_legacy_value", 'multivalued' => FALSE, 'type' => "string" ),
+        "Location" => array( 'map_name' => "Location", 'solr_name' => "map_location", 'multivalued' => TRUE, 'type' => "string" ),
+        "Longitude" => array( 'map_name' => "Longitude", 'solr_name' => "map_longitude", 'multivalued' => FALSE, 'type' => "location" ),
+        "Materials/Techniques" => array( 'map_name' => "Materials/Techniques", 'solr_name' => "map_materials_techniques", 'multivalued' => TRUE, 'type' => "string" ),
+        "Measurement" => array( 'map_name' => "Measurement", 'solr_name' => "map_measurement", 'multivalued' => TRUE, 'type' => "string" ),
+        "Measurement_Dimension" => array( 'map_name' => "Measurement_Dimension", 'solr_name' => "map_measurement_dimension", 'multivalued' => FALSE, 'type' => "string" ),
+        "Measurement_Unit" => array( 'map_name' => "Measurement_Unit", 'solr_name' => "map_measurement_unit", 'multivalued' => FALSE, 'type' => "string" ),
+        "Metadata Update Date" => array( 'map_name' => "Metadata Update Date", 'solr_name' => "map_metadata_update_date", 'multivalued' => FALSE, 'type' => "date" ),
+        "Notes" => array( 'map_name' => "Notes", 'solr_name' => "map_notes", 'multivalued' => TRUE, 'type' => "string" ),
+        "OCR Text" => array( 'map_name' => "OCR Text", 'solr_name' => "map_ocr_text", 'multivalued' => TRUE, 'type' => "string" ),
+        "PreservationCollectionID" => array( 'map_name' => "PreservationCollectionID", 'solr_name' => "map_preservationcollectionid", 'multivalued' => FALSE, 'type' => "string" ),
+        "PreservationItemID" => array( 'map_name' => "PreservationItemID", 'solr_name' => "map_preservationitemid", 'multivalued' => FALSE, 'type' => "string" ),
+        "PreservationSystem" => array( 'map_name' => "PreservationSystem", 'solr_name' => "map_preservationsystem", 'multivalued' => FALSE, 'type' => "string" ),
+        "Provenance" => array( 'map_name' => "Provenance", 'solr_name' => "map_provenance", 'multivalued' => TRUE, 'type' => "string" ),
+        "Publish to Portal" => array( 'map_name' => "Publish to Portal", 'solr_name' => "map_publish_to_portal", 'multivalued' => FALSE, 'type' => "boolean" ),
+        "Record Creation Date" => array( 'map_name' => "Record Creation Date", 'solr_name' => "map_record_creation_date", 'multivalued' => FALSE, 'type' => "date" ),
+        "Record Creator" => array( 'map_name' => "Record Creator", 'solr_name' => "map_record_creator", 'multivalued' => TRUE, 'type' => "string" ),
+        "Record Update Date" => array( 'map_name' => "Record Update Date", 'solr_name' => "map_record_update_date", 'multivalued' => TRUE, 'type' => "date" ),
+        "Record Updator" => array( 'map_name' => "Record Updator", 'solr_name' => "map_record_updator", 'multivalued' => TRUE, 'type' => "string" ),
+        "References" => array( 'map_name' => "References", 'solr_name' => "map_references", 'multivalued' => TRUE, 'type' => "string" ),
+        "Related Work" => array( 'map_name' => "Related Work", 'solr_name' => "map_related_work", 'multivalued' => TRUE, 'type' => "string" ),
+        "Relationships" => array( 'map_name' => "Relationships", 'solr_name' => "map_relationships", 'multivalued' => TRUE, 'type' => "string" ),
+        "Repository" => array( 'map_name' => "Repository", 'solr_name' => "map_repository", 'multivalued' => FALSE, 'type' => "string" ),
+        "Rights" => array( 'map_name' => "Rights", 'solr_name' => "map_rights", 'multivalued' => FALSE, 'type' => "string" ),
+        "Set Title" => array( 'map_name' => "Set Title", 'solr_name' => "map_set_title", 'multivalued' => FALSE, 'type' => "string" ),
+        "Shared Shelf Collection Digital Publisher" => array( 'map_name' => "Shared Shelf Collection Digital Publisher", 'solr_name' => "map_shared_shelf_collection_digital_publisher", 'multivalued' => FALSE, 'type' => "string" ),
+        "Shared Shelf Collection Publication Date" => array( 'map_name' => "Shared Shelf Collection Publication Date", 'solr_name' => "map_shared_shelf_collection_publication_date", 'multivalued' => FALSE, 'type' => "date" ),
+        "Shared Shelf Collection Status" => array( 'map_name' => "Shared Shelf Collection Status", 'solr_name' => "map_shared_shelf_collection_status", 'multivalued' => FALSE, 'type' => "string" ),
+        "Site" => array( 'map_name' => "Site", 'solr_name' => "map_site", 'multivalued' => TRUE, 'type' => "string" ),
+        "Source" => array( 'map_name' => "Source", 'solr_name' => "map_source", 'multivalued' => TRUE, 'type' => "string" ),
+        "Species" => array( 'map_name' => "Species", 'solr_name' => "map_species", 'multivalued' => TRUE, 'type' => "string" ),
+        "SSID" => array( 'map_name' => "SSID", 'solr_name' => "map_ssid", 'multivalued' => FALSE, 'type' => "string" ),
+        "Style/Period" => array( 'map_name' => "Style/Period", 'solr_name' => "map_style_period", 'multivalued' => TRUE, 'type' => "string" ),
+        "Subject" => array( 'map_name' => "Subject", 'solr_name' => "map_subject", 'multivalued' => TRUE, 'type' => "string" ),
+        "Thumbnail" => array( 'map_name' => "Thumbnail", 'solr_name' => "map_thumbnail", 'multivalued' => TRUE, 'type' => "int" ),
+        "Title" => array( 'map_name' => "Title", 'solr_name' => "map_title", 'multivalued' => FALSE, 'type' => "string" ),
+        "Title_Language" => array( 'map_name' => "Title_Language", 'solr_name' => "map_title_language", 'multivalued' => FALSE, 'type' => "string" ),
+        "Transcription" => array( 'map_name' => "Transcription", 'solr_name' => "map_transcription", 'multivalued' => FALSE, 'type' => "string" ),
+        "Translation" => array( 'map_name' => "Translation", 'solr_name' => "map_translation", 'multivalued' => FALSE, 'type' => "string" ),
+        "Venue" => array( 'map_name' => "Venue", 'solr_name' => "map_venue", 'multivalued' => TRUE, 'type' => "string" ),
+        "Volume/Issue" => array( 'map_name' => "Volume/Issue", 'solr_name' => "map_volume_issue", 'multivalued' => FALSE, 'type' => "string" ),
+        "Work Sequence" => array( 'map_name' => "Work Sequence", 'solr_name' => "map_work_sequence", 'multivalued' => FALSE, 'type' => "string" ),
+        "Work Type" => array( 'map_name' => "Work Type", 'solr_name' => "map_work_type", 'multivalued' => TRUE, 'type' => "string" ),
+    );
+
+    function __construct($ss) {
+        $this->sss = $ss; // fully built SharedShelfService object
+    }
+
+    function set_project($project_id) {
+        try {
+            $this->project = $project_id;
+            $this->metadata = $this->sss->project_metadata($this->project);
+            $this->project_fields();
+        }
+        catch (Exception $e) {
+            echo 'set_project caught exception: ',  $e->getMessage(), "\n";
+        } 
+    }
+
+    private function project_fields() {
+        // map sharedshelf field names to MAP field names
+        //print_r($this->metadata);
+        foreach ($this->metadata['columns'] as $column) {
+            $map_field = $column['header'];
+            $map_field_base = preg_replace('/[0-9]+/', '', $map_field);
+            $ss_field = $column['dataIndex'];
+            echo "$ss_field is $map_field base $map_field_base\n";
+            if (isset($this->map_fields["$map_field_base"])) {
+                echo "found\n";
+            }
+        }
+
+        // eliminate unindexed fields
+    }
+
+}
+?>
