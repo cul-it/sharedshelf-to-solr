1d8b22d21e556394714ea501bfa490aa238582cb
James Reidy <jreidy@cornell.edu>
2018-10-22 13:33:46 -0400

    Reformat crosswalk into csv lines.

--- SharedShelfMap/crosswalk-map-ini-solr.php
+++ SharedShelfMap/crosswalk-map-ini-solr.php
@@ -19,41 +19,50 @@ try {
     if (!$ss->logged_in()) {
       throw new Exception("Can not log in.", 1);
     }
+ 
+    // get solr fields from .ini files 
+    $solr_field_list = getSolrFieldList();
     
     $map = new SharedShelfMetadataApplicationProfile($ss);
 
-    $fields = $map->listFields();
-
-    // remove any map_ prefixes from these
-    $map_fields = array();
-    foreach($fields as $field) {
-        $map_fields[] = preg_replace('/^map_/','',$field);
+    $mapFields = $map->mapSolrFields();
+    foreach ($mapFields as $line) {
+        //echo $line;
+        list($map,$solr,$multi) = explode(',', $line);
+        $isnew = isset($solr_field_list["$solr"]) ? 'recycled' : 'new';
+        echo implode(',', array($map,$solr,trim($multi),$isnew)) . "\n";
     }
-    sort($map_fields);
+
+    // $fields = $map->listFields();
+
+    // // remove any map_ prefixes from these
+    // $map_fields = array();
+    // foreach($fields as $field) {
+    //     $map_fields[] = preg_replace('/^map_/','',$field);
+    // }
+    // sort($map_fields);
      
-    // get solr fields from .ini files 
-    $solr_field_list = getSolrFieldList();
 
-    $missing_in_solr = array();
-    $present_in_solr = array();
-    foreach ($map_fields as $map) {
-        if (isset($solr_field_list["$map"])) {
-            $present_in_solr[] = $map;
-        }
-        else {
-            $missing_in_solr[] = $map;
-        }
-    }
+    // $cross = array();
+    // $missing_in_solr = array();
+    // $present_in_solr = array();
+    // foreach ($mapFields as $map) {
+    //     $solr = $map['solr'];
+    //     $map['new_solr'] = isset($solr_field_list["$solr"]) ? $solr : '';
+    //     $map['map'] = '"' . $map['map'] . '"';
+    //     $cross[] = implode(',', $map);
+    // }
 
-    echo "\n\nMAP and solr .ini files both have these fields:\n\n";
-    foreach($present_in_solr as $field) {
-        echo "$field\n";
-    }
+    // echo "\n\nMAP and solr .ini files both have these fields:\n\n";
+    // foreach($present_in_solr as $field) {
+    //     echo "$field\n";
+    // }
 
-    echo "\n\nSolr .ini files are missing these fields:\n\n";
-    foreach($missing_in_solr as $field) {
-        echo "$field\n";
-    }
+    // echo "\n\nSolr .ini files are missing these fields:\n\n";
+    // foreach($missing_in_solr as $field) {
+    //     echo "$field\n";
+    // }
+    // print_r($cross);
 }
 catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
