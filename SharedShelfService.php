34dab80641c29bbad0182722198fe6e8bb5b6cfa
James Reidy <jreidy@cornell.edu>
2018-10-26 10:20:09 -0400

    remove exceptions for missing original filename

--- SharedShelfService.php
+++ SharedShelfService.php
@@ -518,7 +518,8 @@ class SharedShelfService {
     }
     $details = $this->get_response("/assets/$asset_id/representation/details");
     if (empty($details['name'])) {
-      throw new Exception("Missing original filename in media_file_extension().", 1);
+      return '';
+      //throw new Exception("Missing original filename in media_file_extension().", 1);
     }
     // filename of original file
     $filename = $details['name'];
@@ -535,7 +536,8 @@ class SharedShelfService {
     }
     $details = $this->get_response("/assets/$asset_id/representation/details");
     if (empty($details['name'])) {
-      throw new Exception("Missing original filename in media_file_extension().", 1);
+      return '';
+      //throw new Exception("Missing original filename in media_file_extension().", 1);
     }
     // filename of original file
     $filename = $details['name'];
