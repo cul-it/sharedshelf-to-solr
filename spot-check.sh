e6d552f646e6105d97661e63d694d6d95e062f66
James Reidy <jreidy@cornell.edu>
2020-02-13 11:02:12 -0500

    reformat output to line up the dates

--- spot-check.sh
+++ spot-check.sh
@@ -22,11 +22,11 @@ SOLRPRODDATE=`curl -s "http://digcoll.${INTERNAL}library.cornell.edu/solr/digita
 SOLRPDEVDATE=`curl -s "http://digcoll-dev.${INTERNAL}library.cornell.edu:8983/solr/digitalcollections2/select?q=id%3Ass\%3A$ASSET&fl=timestamp&wt=json&indent=true" | grep -E "\"timestamp\":\"[^\}]+" -o | cut -d '"' -f 4`
 PDFDATE=`aws s3 ls sharedshelftosolr.library.cornell.edu/public/$PROJECT/$FILENAME | cut -d ' ' -f 1,2`
 IIIFDATE=`aws s3 ls sharedshelftosolr.library.cornell.edu/public/$PROJECT/$ASSET/image/info.json | cut -d ' ' -f 1,2`
-printf "%34s%30s\n" "Asset ID:" $ASSET
-printf "%34s%30s\n" "Project ID:" $PROJECT
-printf "%34s%30s\n" "Original Filename:" "$FILENAME"
-printf "%34s%30s\n" "Last updated in Forum:" "$FORUMDATE"
-printf "%34s%30s\n" "Last updated in solr production:" "$SOLRPRODDATE"
-printf "%34s%30s\n" "Last updated in solr dev:" "$SOLRPDEVDATE"
-printf "%34s%30s\n" "Date of PDF in S3:" "$PDFDATE"
-printf "%34s%30s\n" "Date of IIIF in S3:" "$IIIFDATE"
+printf "%34s%-30s\n" "Asset ID: " $ASSET
+printf "%34s%-30s\n" "Project ID: " $PROJECT
+printf "%34s%-30s\n" "Original Filename: " "$FILENAME"
+printf "%34s%-30s\n" "Last updated in Forum: " "$FORUMDATE"
+printf "%34s%-30s\n" "Last updated in solr production: " "$SOLRPRODDATE"
+printf "%34s%-30s\n" "Last updated in solr dev: " "$SOLRPDEVDATE"
+printf "%34s%-30s\n" "Date of PDF in S3: " "$PDFDATE"
+printf "%34s%-30s\n" "Date of IIIF in S3: " "$IIIFDATE"
