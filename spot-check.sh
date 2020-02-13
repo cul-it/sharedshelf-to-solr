04cc2bebd8ea5880265c4a0ac9c1df1dd20028ba
James Reidy <jreidy@cornell.edu>
2020-02-13 11:01:45 -0500

    use internal and then port for dev solr

--- spot-check.sh
+++ spot-check.sh
@@ -18,8 +18,8 @@ FORUM=`php ssAssetTest.php -s $ASSET`
 PROJECT=`echo $FORUM | grep -E "\[project_id[^\[]+" -o | grep -E "[0-9]+" -o`
 FILENAME=`echo $FORUM | grep -E "\[filename[^\[]+" -o | cut -d ' ' -f 3`
 FORUMDATE=`echo $FORUM | grep -E "\[updated_on[^\[\)]+" -o | cut -d ' ' -f 3`
-SOLRPRODDATE=`curl -s "http://digcoll.internal.library.cornell.edu/solr/digitalcollections2/select?q=id%3Ass\%3A$ASSET&fl=timestamp&wt=json&indent=true" | grep -E "\"timestamp\":\"[^\}]+" -o | cut -d '"' -f 4`
-SOLRPDEVDATE=`curl -s "http://digcoll-dev.internal.library.cornell.edu/solr/digitalcollections2/select?q=id%3Ass\%3A$ASSET&fl=timestamp&wt=json&indent=true" | grep -E "\"timestamp\":\"[^\}]+" -o | cut -d '"' -f 4`
+SOLRPRODDATE=`curl -s "http://digcoll.${INTERNAL}library.cornell.edu/solr/digitalcollections2/select?q=id%3Ass\%3A$ASSET&fl=timestamp&wt=json&indent=true" | grep -E "\"timestamp\":\"[^\}]+" -o | cut -d '"' -f 4`
+SOLRPDEVDATE=`curl -s "http://digcoll-dev.${INTERNAL}library.cornell.edu:8983/solr/digitalcollections2/select?q=id%3Ass\%3A$ASSET&fl=timestamp&wt=json&indent=true" | grep -E "\"timestamp\":\"[^\}]+" -o | cut -d '"' -f 4`
 PDFDATE=`aws s3 ls sharedshelftosolr.library.cornell.edu/public/$PROJECT/$FILENAME | cut -d ' ' -f 1,2`
 IIIFDATE=`aws s3 ls sharedshelftosolr.library.cornell.edu/public/$PROJECT/$ASSET/image/info.json | cut -d ' ' -f 1,2`
 printf "%34s%30s\n" "Asset ID:" $ASSET
